83f16c97b903292d8985a53aef0361698defed4a
Vijay Kesanakurthi <151064524+Prathyusha1211@users.noreply.github.com>
2024-08-19 15:35:46 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
         // matching all API routes
         source: "/api/(.*)",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
