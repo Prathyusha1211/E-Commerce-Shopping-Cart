445c6d08589da37df7cbd7ef34c2af367b545507
Vijay Kesanakurthi <151064524+Prathyusha1211@users.noreply.github.com>
2024-08-19 12:06:14 +0530

    Add. Loading

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import ProductCard from "@/components/product_card";
 import { getProducts } from "@/lib/services";
+import { useEffect, useState } from "react";
 
 export default async function Page() {
   // return <div>API URL not found</div>
@@ -7,18 +8,34 @@ export default async function Page() {
     return <div>API URL not found</div>;
   }
 
-  const products = await getProducts();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getProducts()
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div>
-      <div className="bg-slate-100 container mx-auto p-8">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          {console.log(products)}
-          {products.map((product, index) => (
-            <ProductCard key={index} product={product} />
+      {loading ? (
+        <div className="text-center py-10">
+          <div className="loader">Loading...</div>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
           ))}
         </div>
-      </div>
+      )}
     </div>
   );
 }
